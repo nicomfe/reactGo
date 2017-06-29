b5163f4e43048fa137b7925bb3e1fefc8ceac1c9
Nicolas Fetter <nicofetter@gmail.com>
2017-06-29 19:06:12 +1200

    Adding menu bar

--- app/containers/Navigation.js
+++ app/containers/Navigation.js
@@ -4,6 +4,7 @@ import ImmutablePropTypes from 'react-immutable-proptypes'
 import { Link } from 'react-router'
 import { connect } from 'react-redux'
 import classNames from 'classnames/bind'
+import AppBar from 'material-ui/AppBar'
 
 import * as userActions from '../actions/users'
 import Avatar from '../components/Avatar'
@@ -13,27 +14,32 @@ const cx = classNames.bind(styles)
 
 const Navigation = ({ user, logOut }) => {
   if (!user) return null
+
+  const getLinks = () => {
+    return (
+      <div className={cx('displayFlex')}>
+        <Link className={cx('item')} activeClassName={cx('active')} to="/dashboard">Dashboard</Link>
+        <Link to="/about" className={cx('item')} activeClassName={cx('active')}>About</Link>
+        { user.get('authenticated') ? (
+          <Link
+            onClick={logOut}
+            className={cx('item')} to="/"
+          >Logout</Link>
+        ) : (
+          <Link className={cx('item')} to="/login">Log in</Link>
+        )}
+
+        {user.get('authenticated') ? (<Avatar image={user.getIn(['json', 'profile', 'picture'])} />) : null}
+      </div>
+    )
+  }
+
   return (
-    <nav className={cx('navigation')} role="navigation">
-      <Link
-        to="/"
-        className={cx('item', 'logo')}
-        activeClassName={cx('active')}
-      >React Boilerplate</Link>
-
-      <Link className={cx('item')} to="/dashboard">Dashboard</Link>
-      <Link to="/about" className={cx('item')} activeClassName={cx('active')}>About</Link>
-      { user.get('authenticated') ? (
-        <Link
-          onClick={logOut}
-          className={cx('item')} to="/"
-        >Logout</Link>
-      ) : (
-        <Link className={cx('item')} to="/login">Log in</Link>
-      )}
-
-      {user.get('authenticated') ? (<Avatar image={user.getIn(['json', 'profile', 'picture'])} />) : null}
-    </nav>
+    <AppBar
+      title="React Boilerplate"
+      iconElementRight={getLinks()}
+      className={cx('menuBar')}
+    />
   )
 }
 
