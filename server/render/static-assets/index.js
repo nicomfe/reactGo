599f267e70bdbc12489582cb8cd7bebe34a93db4
Nicolas Fetter <nicofetter@gmail.com>
2017-07-05 19:05:44 +1200

    Attempt to fix buikld

--- server/render/static-assets/index.js
+++ server/render/static-assets/index.js
@@ -1,3 +1,3 @@
-const createStaticAssets = process.env.__PRODUCTION__ ? require('./prod') : require('./dev')
+const createStaticAssets = __PRODUCTION__ ? require('./prod') : require('./dev')
 
 export default createStaticAssets
